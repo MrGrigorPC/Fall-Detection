c2adbbd941669be95be727b4ddf2643d5205ef90
MrGrigorPC <grisha.esayan@mail.ru>
2023-09-09 02:08:56 +0400

    Oops, forgot to remove print shape statements

--- fall_detection.py
+++ fall_detection.py
@@ -4,7 +4,7 @@ skeleton_1 = np.load('./data/skeleton_3.npy')
 
 
 class FallDetecion:
-    def __init__(self,fps=18):
+    def __init__(self, fps=18):
         self.torso_up = np.array(
             [5, 6]
         )  # The slice used for generating the midpoint of the shoulders
@@ -50,15 +50,12 @@ class FallDetecion:
         difference = np.subtract(
             vectors[:, :, :, 0], vectors[:, :, :, 1]
         )  # Subtracts the coordinates to obtain the vectors
-        print(difference.shape)
         dot = (difference[:, :, 0, :] * difference[:, :, 1, :]).sum(
             axis=-1
         )  # Calculates the dot product between the pairs of vectors
-        print(dot.shape)
         norm = np.prod(
             np.linalg.norm(difference[:, :, :, :], axis=3), axis=-1
         )  # Calculates the norm of the vectors and multiplies them, same as |a|*|b|
-        print(norm.shape)
 
         cos_angle = np.divide(dot, norm)  # cos(x) = dot(a,b)/|a|*|b|
 
@@ -79,10 +76,14 @@ class FallDetecion:
 
         keypoints[keypoints < 0] = np.nan
         # Handle missing values
-        torso_up = keypoints[:, self.torso_up].mean(axis=1)[:, np.newaxis] # Getting middle point of torso down and adding new axis for further concatenation
-        torso_down = keypoints[:, self.torso_down].mean(axis=1)[:, np.newaxis] # Getting middle point of torso up and adding new axis for further concatenation
-        head_coordinate = np.nanmean(keypoints[:, :5], axis=1)[:, np.newaxis] # Getting mean value of head coordinate and adding new axis for further concatenation
-        vertical = np.tile(self.vertical_coordinates, (keypoints.shape[0], 1, 1)) # Numpy broadcasting for further concatenation
+        torso_up = keypoints[:, self.torso_up].mean(axis=1)[:,
+                   np.newaxis]  # Getting middle point of torso down and adding new axis for further concatenation
+        torso_down = keypoints[:, self.torso_down].mean(axis=1)[:,
+                     np.newaxis]  # Getting middle point of torso up and adding new axis for further concatenation
+        head_coordinate = np.nanmean(keypoints[:, :5], axis=1)[:,
+                          np.newaxis]  # Getting mean value of head coordinate and adding new axis for further concatenation
+        vertical = np.tile(self.vertical_coordinates,
+                           (keypoints.shape[0], 1, 1))  # Numpy broadcasting for further concatenation
         keypoints = np.concatenate((
             keypoints,
             torso_up,
@@ -105,13 +106,13 @@ class FallDetecion:
                 - float: fallScore
         '''
 
-
         isFall = False
         keypoints = self.collectData(skeleton)
         vector_pairs = keypoints[:, self.vector_indices][:, self.pair_indices]
         angles = self.angleCalculation(vector_pairs)
-        for i in range(len(angles)-27):
-            fallScore = np.nanmean(np.abs(np.diff(angles[i:i+26], axis=0)) * self.fps) # Making prediction using 27 frames(1.5 sec)
+        for i in range(len(angles) - 27):
+            fallScore = np.nanmean(
+                np.abs(np.diff(angles[i:i + 26], axis=0)) * self.fps)  # Making prediction using 27 frames(1.5 sec)
             if fallScore > 125:
                 isFall = True
                 return isFall, fallScore
